ecb53208de7a6949d50f79eb6929884b25229d5d
Garrett Brown <garbearucla@gmail.com>
2013-09-11 22:47:48 -0700

    [game clients] Added new Game Client add-ons based on the libretro API.
    Thanks to Themaister for rewind functionality and fetzerch for file length check.

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -276,6 +276,9 @@
 #include "utils/XMLUtils.h"
 #include "addons/AddonInstaller.h"
 
+/* Game-related include files */
+#include "games/GameManager.h"
+
 #ifdef HAS_PERFORMANCE_SAMPLE
 #include "utils/PerformanceSample.h"
 #else
@@ -1468,6 +1471,8 @@ bool CApplication::Initialize()
   CJoystickManager::Get().SetEnabled(CSettings::Get().GetBool("input.enablejoystick") &&
                     CPeripheralImon::GetCountOfImonsConflictWithDInput() == 0);
 #endif
+
+  GAMES::CGameManager::Get().Start();
 
   return true;
 }
@@ -3262,6 +3267,7 @@ bool CApplication::Cleanup()
     g_windowManager.Remove(WINDOW_SETTINGS_SERVICE);
     g_windowManager.Remove(WINDOW_SETTINGS_APPEARANCE);
     g_windowManager.Remove(WINDOW_SETTINGS_MYPVR);
+    g_windowManager.Remove(WINDOW_SETTINGS_MYGAMES);
     g_windowManager.Remove(WINDOW_DIALOG_KAI_TOAST);
 
     g_windowManager.Remove(WINDOW_DIALOG_SEEK_BAR);
@@ -3373,6 +3379,7 @@ void CApplication::Stop(int exitCode)
     CApplicationMessenger::Get().Cleanup();
 
     StopPVRManager();
+    GAMES::CGameManager::Get().Stop();
     StopServices();
     //Sleep(5000);
 
