c5256d03051c237c95728d0591dc9317b51f060c
ace20022 <ace20022@ymail.com>
2014-12-03 19:26:56 +0100

    [bluray] Treat return value 0 as error when seeking.

--- xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -922,7 +922,7 @@ int CDVDInputStreamBluray::GetTime()
 
 bool CDVDInputStreamBluray::PosTime(int ms)
 {
-  if(m_dll->bd_seek_time(m_bd, ms * 90) < 0)
+  if(m_dll->bd_seek_time(m_bd, ms * 90) <= 0)
     return false;
   else
     return true;
