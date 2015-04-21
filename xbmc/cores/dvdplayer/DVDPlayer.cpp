b4cfc73d0828fdfd179e2696ae55b9de70285e0f
ace20022 <ace20022@ymail.com>
2015-04-21 22:46:25 +0200

    [bluray] Clear (menu) overlays after seek.

--- xbmc/cores/dvdplayer/DVDPlayer.cpp
+++ xbmc/cores/dvdplayer/DVDPlayer.cpp
@@ -3756,6 +3756,10 @@ int CDVDPlayer::OnDVDNavResult(void* pData, int iMessage)
         CLog::Log(LOGDEBUG, "CDVDPlayer::OnDVDNavResult - libbluray DVDSTATE_STILL end");
       }
     }
+    else if (iMessage == 9)
+    {
+      m_overlayContainer.Clear();
+    }
     return 0;
   }
 
