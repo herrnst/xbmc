c3123080d6ce87922dceaa578bb6db4ef53aea62
Daniel Scheller <d.scheller@gmx.net>
2021-01-05 16:27:26 +0100

    Settings: Restart media handling when toggling the allowrtpassthrough option
    Register a callback for the allowrtpassthrough option and trigger a
    TMSG_MEDIA_RESTART event to restart the media/audio handling if the option
    is toggled, so that passthrough will eventually be directly enabled if
    appropriate.

--- xbmc/application/ApplicationSettingsHandling.cpp
+++ xbmc/application/ApplicationSettingsHandling.cpp
@@ -53,6 +53,7 @@ void CApplicationSettingsHandling::RegisterSettings()
 
   settingsMgr->RegisterCallback(this, {
                                           CSettings::SETTING_AUDIOOUTPUT_PASSTHROUGH,
+                                          CSettings::SETTING_AUDIOOUTPUT_ALLOWRTPASSTHROUGH,
                                           CSettings::SETTING_LOOKANDFEEL_SKIN,
                                           CSettings::SETTING_LOOKANDFEEL_SKINSETTINGS,
                                           CSettings::SETTING_LOOKANDFEEL_FONT,
@@ -139,6 +140,10 @@ void CApplicationSettingsHandling::OnSettingChanged(const std::shared_ptr<const 
           CServiceBroker::GetWinSystem()->GetGfxContext().GetVideoResolution(), true);
   }
   else if (settingId == CSettings::SETTING_AUDIOOUTPUT_PASSTHROUGH)
+  {
+    CServiceBroker::GetAppMessenger()->PostMsg(TMSG_MEDIA_RESTART);
+  }
+  else if (settingId == CSettings::SETTING_AUDIOOUTPUT_ALLOWRTPASSTHROUGH)
   {
     CServiceBroker::GetAppMessenger()->PostMsg(TMSG_MEDIA_RESTART);
   }
