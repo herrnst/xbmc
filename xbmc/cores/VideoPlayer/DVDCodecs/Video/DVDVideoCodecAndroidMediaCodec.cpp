6a0bd2ab3c9d0f4f46fbdd732989c142eceff7ab
quietvoid <39477805+quietvoid@users.noreply.github.com>
2022-09-10 13:09:01 -0400

    Add support for Matroska based DOVI config

--- xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAndroidMediaCodec.cpp
+++ xbmc/cores/VideoPlayer/DVDCodecs/Video/DVDVideoCodecAndroidMediaCodec.cpp
@@ -516,7 +516,9 @@ bool CDVDVideoCodecAndroidMediaCodec::Open(CDVDStreamInfo &hints, CDVDCodecOptio
       m_mime = "video/hevc";
       m_formatname = "amc-hevc";
 
-      bool isDvhe = (m_hints.codec_tag == MKTAG('d', 'v', 'h', 'e'));
+      bool isDvhe = (m_hints.codec_tag == MKTAG('d', 'v', 'h', 'e') ||
+                     m_hints.codec_tag == MKBETAG('d', 'v', 'c', 'C') ||
+                     m_hints.codec_tag == MKBETAG('d', 'v', 'v', 'C'));
       bool isDvh1 = (m_hints.codec_tag == MKTAG('d', 'v', 'h', '1'));
 
       // some files don't have dvhe or dvh1 tag set up but have Dolby Vision side data
